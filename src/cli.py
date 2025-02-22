611c0ada99bdb895984efe5da0aee3fe8b73a417
etatarintsev <evtatarintsev@ya.ru>
2025-02-22 11:49:30 +0300

    add first mypy config

--- src/cli.py
+++ src/cli.py
@@ -2,9 +2,11 @@ from pathlib import Path
 
 import click
 
+from sponsr.sponsr_post import SponsrPost
+
 
 @click.group("dump")
-def cli():
+def cli() -> None:
     ...
 
 
@@ -13,9 +15,9 @@ def cli():
 @click.option('--dest-dir', "-d",
               type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True),
               help="Директория для загрузки файла")
-def video(url: str, dest_dir: str):
+def video(url: str, dest_dir: str) -> None:
     """Сохраняет видео по указанному url в директорию dest."""
-    ...
+    SponsrPost(url)
 
 
 if __name__ == '__main__':
