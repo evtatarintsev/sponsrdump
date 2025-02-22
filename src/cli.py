a4a8a073c2a4a096c17d91b53a81dff8434e428a
etatarintsev <evtatarintsev@ya.ru>
2025-02-22 11:36:31 +0300

    check dest dir

--- src/cli.py
+++ src/cli.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import click
 
 
@@ -8,8 +10,10 @@ def cli():
 
 @cli.command()
 @click.option("--url", "-url", required=True, help='URL видео на sponsr.ru.')
-@click.option('--dest-dir', "-d", help="Директория для загрузки файла")
-def video(url: str, dest: str):
+@click.option('--dest-dir', "-d",
+              type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True),
+              help="Директория для загрузки файла")
+def video(url: str, dest_dir: str):
     """Сохраняет видео по указанному url в директорию dest."""
     ...
 
